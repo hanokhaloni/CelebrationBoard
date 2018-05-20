be7d07dffc4d29ee799fd6ba75987306b1e23aae
Henry <hanokh.aloni@gmail.com>
2018-05-20 11:46:17 +0300

    Added initial vanilla.js construct. Added jasmine test.

---
+++ celebrationgrid_test.js
@@ -0,0 +1,11 @@
+describe("Celebration Grid", function() {
+  describe("General functional test", function() {
+    it("should be true", function() {
+      expect(true);
+    });
+    
+  });   
+});   
+jasmine.getEnv().addReporter(new jasmine.TrivialReporter());;
+jasmine.getEnv().execute();
+  
